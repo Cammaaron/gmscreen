43f8dcd67ad4fc25b5e3cd50a1a6e00f304c781d
Aaron <Cammaaron@gmail.com>
2018-02-12 12:07:37 -0500

    Added properties for MySQL

--- src/main/java/com/cammaaron/gmscreen/GmscreenApplication.java
+++ src/main/java/com/cammaaron/gmscreen/GmscreenApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GmscreenApplication {
 
 	public static void main(String[] args) {
+		System.out.println(String.format("Database url set at %s with username %s", System.getenv("DATASOURCE_URL"), System.getenv("DATASOURCE_USERNAME")));
 		SpringApplication.run(GmscreenApplication.class, args);
 	}
 }
