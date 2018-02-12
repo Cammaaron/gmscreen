cd2146620e79ce5d3865201b460833bff279f70b
Aaron <Cammaaron@gmail.com>
2018-02-12 07:57:27 -0500

    init commit

---
+++ src/main/java/com/cammaaron/gmscreen/GmscreenApplication.java
@@ -0,0 +1,12 @@
+package com.cammaaron.gmscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmscreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmscreenApplication.class, args);
+	}
+}
---
+++ src/main/java/com/cammaaron/gmscreen/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cammaaron.gmscreen;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GmscreenApplication.class);
+	}
+
+}
